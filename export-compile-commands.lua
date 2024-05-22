863bface7cb634573d483a17410a78b2dc51cfb6
Jarod42 <Jarod42@users.noreply.github.com>
2024-05-22 18:06:04 +0200

    Replace deprecated `sysincludedirs` by `externalincludedirs`.

--- export-compile-commands.lua
+++ export-compile-commands.lua
@@ -37,7 +37,7 @@ function m.getCommonFlags(prj, cfg)
   local flags = toolset.getcppflags(cfg)
   flags = table.join(flags, toolset.getdefines(cfg.defines))
   flags = table.join(flags, toolset.getundefines(cfg.undefines))
-  flags = table.join(flags, toolset.getincludedirs(cfg, cfg.includedirs, cfg.sysincludedirs))
+  flags = table.join(flags, toolset.getincludedirs(cfg, cfg.includedirs, cfg.externalincludedirs))
   flags = table.join(flags, toolset.getforceincludes(cfg))
   if project.iscpp(prj) then
     flags = table.join(flags, toolset.getcxxflags(cfg))
